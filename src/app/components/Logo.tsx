90644f17be3d856cfe82e90101e0633b50444f95
JesusC-R <cuevasrodartej@gmail.com>
2023-09-09 02:30:40 -0700

    Feat: Add animations to button and main image.

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,31 +1,22 @@
-"use client";
-
 import Link from "next/link";
 import { motion } from "framer-motion";
 
 const Logo = () => {
   const MotionLink = motion(Link);
 
+  const buttonVariants = {
+    initial: { scale: 1, opacity: 0.8 },
+    hover: { scale: 1.05, opacity: 1 },
+  };
+
   return (
     <div className="flex items-center justify-center mt-2">
       <MotionLink
         href="/"
-        className="h-16 w-16 bg-dark text-light flex
-           items-center justify-center rounded-full text-2xl font-bold"
-        whileHover={{
-          backgroundColor: [
-            "#121212",
-            "rgba(131,58,180,1)",
-            "rgba(253,29,29,1)",
-            "rgba(252,176,69,1)",
-            "rgba(131,58,180,1)",
-            "#121212",
-          ],
-          transition: {
-            duration: 1,
-            repeat: Infinity,
-          },
-        }}
+        className="h-16 w-16 bg-gray-700 text-white rounded-full p-4 flex items-center justify-center text-2xl font-bold transition-transform transform hover:scale-105"
+        variants={buttonVariants}
+        initial="initial"
+        whileHover="hover"
       >
         JCR
       </MotionLink>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import Layout from "./components/Layout";
+import profilePic from "../../public/images/profile/developer-pic-1.png";
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <main className="flex items-center text-dark w-full min-h-screen">
+      <Layout className="pt-0">
+        <div className="flex items-center justify-between w-full image-animation">
+          <div className="w-1/2">
+            <Image src={profilePic} alt="Image" className="w-full h-auto" />
+          </div>
+          <div className="w-1/2">
+            <h1>Turning Vision Into Reality With Code And Design</h1>
+            <p>
+              As a skilled full-stack developer, I am dedicated to turning ideas
+              into inovative web applications. Explore my latest projects and
+              articles, showcasing my expertice in React.js and web development.
+            </p>
+          </div>
+        </div>
+      </Layout>
+    </main>
   );
 }
