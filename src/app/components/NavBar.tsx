8f307d4347ba7a1b192f1de83cfa047912ce5958
JesusC-R <cuevasrodartej@gmail.com>
2023-09-22 17:34:45 -0700

    Feat: Add animation to navigation modal.

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -154,7 +154,14 @@ const NavBar = () => {
 
       {/* Mobile Navbar */}
       {isOpen ? (
-        <div
+        <motion.div
+          initial={{
+            scale: 0,
+            opacity: 0,
+            x: "-50%",
+            y: "-50%",
+          }}
+          animate={{ scale: 1, opacity: 1, }}
           className="min-w-[70vw] flex flex-col justify-between items-center
             fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-dark/50
             dark:bg-light/50 rounded-lg backdrop-blur-xl py-32"
@@ -223,7 +230,7 @@ const NavBar = () => {
               )}
             </motion.button>
           </nav>
-        </div>
+        </motion.div>
       ) : null}
 
       <div className="absolute left-[50%] top-2 translate-x-[50%]">
