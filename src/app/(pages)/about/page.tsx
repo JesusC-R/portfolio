69e7911bd83f39a57cb8c31d29b88cd16e7bea6f
JesusC-R <cuevasrodartej@gmail.com>
2023-09-28 14:54:16 -0700

    Fix: Adjust size of footer in smaller screens.

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -58,10 +58,18 @@ const About = () => {
       </head>
       <div className="flex w-full flex-col items-center justify-center">
         <Layout className="pt-16">
-          <AnimatedText text="Passion Fuels Purpose" className="mb-16" />
-          <div className="grid w-full grid-cols-8 gap-16">
-            <div className="col-span-3 flex flex-col items-start justify-start">
-              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+          <AnimatedText
+            text="Passion Fuels Purpose"
+            className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start
+            xl:col-span-4 md:order-2 md:col-span-8"
+            >
+              <h2
+                className="mb-4 text-lg font-bold uppercase text-dark/75
+                dark:text-light/75"
+              >
                 Biography
               </h2>
               <p className="my-4 font-medium dark:text-light">
@@ -86,7 +94,8 @@ const About = () => {
             </div>
             <div
               className="col-span-3 relative h-max rounded-2xl border-2
-                border-solid border-dark bg-light p-8 dark:border-light dark:bg-dark"
+                border-solid border-dark bg-light p-8 dark:border-light dark:bg-dark
+                xl:col-span-4 md:order-1 md:col-span-8"
             >
               <div
                 className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%]
@@ -101,7 +110,10 @@ const About = () => {
                 33vw"
               />
             </div>
-            <div className="col-span-2 flex flex-col items-end justify-between">
+            <div
+              className="col-span-2 flex flex-col items-end justify-between xl:col-span-8
+                xl:flex-row xl:items-center md:order-3"
+            >
               <div className="flex flex-col items-end justify-center">
                 <span className="inline-block text-7xl font-bold dark:text-light">
                   <AnimatedNumbers value={10} />+
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,12 +8,13 @@ const Footer = () => {
   return (
     <footer
       className="w-full border-t-2 border-solid border-dark
-        font-medium text-lg dark:border-light sm:text-base"
-    > 
+        font-medium text-lg dark:border-light sm:text-base
+        xs:text-xs"
+    >
       <Layout
         className="py-8 flex items-center justify-between dark:text-light
-          lg:flex-col lg:py-6">
-        <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+          lg:flex-col lg:py-6 xs:py-5 xs:justify-start xs:w-[120%] xs:-ml-[1.5rem]">
+        <span className="xs:justify-end">{new Date().getFullYear()} &copy; All Rights Reserved.</span>
         <Link href="/">JCR</Link>
       </Layout>
     </footer>
