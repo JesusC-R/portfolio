2b85d1cf25c1a95462a8fe8c6b1e705772b24962
JesusC-R <cuevasrodartej@gmail.com>
2023-10-04 23:13:24 -0700

    Fix: Adjust paragraph for small screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,10 +35,10 @@ export default function Home() {
           >
             <AnimatedText
               className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl
-                sm:!text-3xl xs:!text-xl"
+                sm:!text-3xl xs:!text-xl sm:inline-block"
               text="Turning Vision Into Reality With Code And Design"
             />
-            <p className="my-4 text-base font-medium md:text-sm sm:text-xs">
+            <p className="my-4 text-base font-medium md:text-sm sm:text-xs sm:p-2">
               As a skilled full-stack developer, I am dedicated to turning ideas
               into inovative web applications. Explore my latest projects and
               articles, showcasing my expertice in React.js and web development.
