62b7151f4523d55aeb5d6222b77256ba53fa0f6a
JesusC-R <cuevasrodartej@gmail.com>
2023-10-06 17:16:19 -0700

    Feat: Add responsiveness to Articles page.

--- src/app/(pages)/articles/page.tsx
+++ src/app/(pages)/articles/page.tsx
@@ -58,7 +58,7 @@ const HoverArticleImg: React.FC<ArticleProps> = ({ title, img, link }) => {
         sizes="(max-width: 768px) 100vw,
           (max-width: 1200px) 50vw,
           33vw"
-        className="w-96 h-auto hidden absolute rounded-lg z-10"
+        className="w-96 h-auto hidden absolute rounded-lg z-10 md:!hidden"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
         style={{ x: x, y: y }}
@@ -80,11 +80,11 @@ const Article: React.FC<ArticleProps> = ({
     return (
       <li
         className="col-span-1 w-full p-4 bg-light border border-solid
-      border-dark rounded-2xl relative dark:bg-dark dark:border-light"
+        border-dark rounded-2xl relative dark:bg-dark dark:border-light"
       >
         <div
           className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%]
-        rounded-[2rem] bg-dark dark:bg-light"
+            rounded-[2rem] bg-dark dark:bg-light"
         />
         <Link
           href={link}
@@ -103,7 +103,10 @@ const Article: React.FC<ArticleProps> = ({
           />
         </Link>
         <Link href={link} target="_blank">
-          <h2 className="capitalize text-2xl font-bold my-2 hover:underline mt-4 dark:text-light">
+          <h2
+            className="capitalize text-2xl font-bold my-2 hover:underline mt-4 dark:text-light
+              xs:text-lg sm:text-xl"
+          >
             {title}
           </h2>
           <p className="text-sm mb-2 dark:text-light">{summary}</p>
@@ -117,13 +120,18 @@ const Article: React.FC<ArticleProps> = ({
       className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center
         justify-between bg-light text-dark first:mt-0 border border-solid
         border-dark border-r-4 border-b-4 dark:bg-dark dark:text-light
-        dark:border-light"
+        dark:border-light sm:flex-col"
       initial={{ y: 200 }}
       whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeOut" } }}
       viewport={{ once: true }}
     >
       <HoverArticleImg title={title} img={img} link={link} />
-      <span className="text-primary font-semibold pl-4 ">{date}</span>
+      <span
+        className="text-primary font-semibold pl-4 sm:self-start sm:pl-0 xs:text-sm
+        "
+        >
+          {date}
+        </span>
     </motion.li>
   );
 };
@@ -137,8 +145,11 @@ const Articles = () => {
       </Head>
       <div className="w-full mb-16 flex flex-col items-center justify-center overflow-hidden">
         <Layout className="pt-16">
-          <AnimatedText text="Words Can Change The World" className="mb-16" />
-          <ul className="grid grid-cols-2 gap-16">
+          <AnimatedText
+            text="Words Can Change The World"
+            className="mb-16 xl:!text-7xl sm:!text-6xl xs:!text-3xl sm:my-8"
+          />
+          <ul className="grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16">
             <Article
               img={articleImg}
               title="Build A Custom Pagination Component In ReactJS From Scratch"
